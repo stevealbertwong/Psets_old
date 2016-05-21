395d65500a00ac184de892ec69626f3fff90bac0
steveandrewwong <wongkwunkit@gmail.com>
2016-05-20 22:43:47 -0700

    new file structure

---
+++ CS1100/web-get.cc
@@ -0,0 +1,26 @@
+/* CS110 revised 19 May 2016
+How to speak HTTP like telnet on the internet
+
+ref:
+https://www.le.ac.uk/users/rjm1/cotter/page_74.htm
+
+
+
+Compare example: http://cboard.cprogramming.com/c-programming/88112-simple-proxy.html
+*/
+
+// steam is a common logical interface to devices
+
+// ?? WHY STATIC
+// statically allocated -> data structure that are fixed in size 
+// + done in compile time 
+// advantages: efficient execution time
+// disadvantages: waste space if redunctant or no space
+
+// ?? what is iosockstream& ss
+static void sentRequest(){
+
+};
+
+static void skipHeader(ios)
+
