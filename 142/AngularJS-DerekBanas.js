0748cb236e759a6569fdbb06bfcd134bc7137ff6
steveandrewwong <wongkwunkit@gmail.com>
2016-06-08 02:16:20 -0700

    Pro Javascript Development.pdf

---
+++ 142/AngularJS-DerekBanas.js
@@ -0,0 +1,21 @@
+/*
+Derek Banas
+
+Module
+- component used in applications
+
+Directive
+- extend HTML tags, bind data to HTML elements
+
+Scope
+- JS object that represents data 
+
+Expressions
+- directly linked to the scope, updated dynamically as data changes 
+
+Services
+- Ajax techniques, 
+
+
+
+*/
---
+++ 142/ProJavaScript-Development.js
@@ -0,0 +1,11 @@
+/*
+
+An object in JavaScript is a standalone entity consisting of one or more related variables and functions, known as
+properties and methods
+
+The simplest way of creating your own object for use in your JavaScript code is to use object literal notation, denoted
+by curly braces, when defining a variable
+
+
+
+*/
