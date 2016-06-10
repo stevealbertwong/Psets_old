794a0787df0e20ba3ebd923bc4249fc161603fe0
steveandrewwong <wongkwunkit@gmail.com>
2016-06-09 23:48:49 -0700

    boolean algebra, Demorgan's law, bitwise operator

--- 107/351/lecture-demo.c
+++ 107/351/lecture-demo.c
@@ -37,8 +37,59 @@ array_ptr = big_array[130]; // 0x0208, point to out of bound, but C doesnt care,
 char HP[13] = "Harry Potter";
 char null_string = HP[13]; // "\0"
 
-// printing data
-void show_bytes(){
+/* 
+cse 351 array lecture 
+printing address + hex representation of value
+*/
+#include <stdio.h>
+
+void show_bytes(int* p, int len){
 	
+	for (int i = 0; i < len; ++i)
+	{
+		// %x: hex %p: pointer *(p+1):gabbage value 
+		// \t:tab \n:newline %.5x: shows 5 digits of hex value
+		printf("%p\t%.5x\t%x\n", p+i, *(p+i), *(p+i));
+
+	};
+
+};
+
+void show_int(int x){
+	// (char*) &x 
+	show_bytes((int*) &x, sizeof(int));
 }
+
+int main(int argc, char const *argv[])
+{
+	int a = 12345; // hex representation of the value-> 0x00003039
+	int* pointer = &a; // have to initialize pointer
+	show_bytes(pointer, 10); // print ptr addr + value
+	printf("here is show_int\n");
+	show_int(a);
+
+	return 0;
+}
+
+
+/*
+
+Bit-level operator
+
+Logic operator 
+
+*/
+
+// bitwise operator &, |, ^, ~
+
+char a,b,c;
+a = (char) 0x00; // 00000000
+b = ~a; // 11111111
+a = (char)0x41; // 0x41 = 01000001
+b = ~a; // 10111110 = 0xBE
+
+
+
+
+
 
