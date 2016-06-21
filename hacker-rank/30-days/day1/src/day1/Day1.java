efdc5209ca4003c2e26cc1ab71687d1eaf3662ec
steveandrewwong <wongkwunkit@gmail.com>
2016-06-21 02:40:06 -0700

    java 30 days system.out.println

---
+++ hacker-rank/30-days/day1/src/day1/Day1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day1;
+import java.io.*; // scanner class
+import java.util.*;
+import java.text.*;
+import java.math.*; // java: library, math: package, *: all classes
+/**
+ *
+ * @author SteveAndrewWong
+ */
+public class Day1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        
+        
+    }
+    
+}
