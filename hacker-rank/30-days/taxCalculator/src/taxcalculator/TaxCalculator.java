43f41509b3f8a0cdefc68bc1e096076d3ce287b7
steveandrewwong <wongkwunkit@gmail.com>
2016-06-21 09:17:43 -0700

    Arraylist

--- hacker-rank/30-days/taxCalculator/src/taxcalculator/TaxCalculator.java
+++ hacker-rank/30-days/taxCalculator/src/taxcalculator/TaxCalculator.java
@@ -8,7 +8,8 @@ public class TaxCalculator {
 
     public static void main(String[] args) {
     
-      Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in); // aka token or input
+      // new Scanner(new File(filename)); 
       double M = sc.nextDouble(); // original meal price
       int T = sc.nextInt(); // tip percentage
       int X = sc.nextInt(); // tax percentage
