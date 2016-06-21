d2adaba45dbc8a0b380e55da6fc6e9c40c613af8
steveandrewwong <wongkwunkit@gmail.com>
2016-06-21 04:55:50 -0700

    taxCalculator

---
+++ hacker-rank/30-days/taxCalculator/src/taxcalculator/TaxCalculator.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TaxCalculator {
+
+    public static void main(String[] args) {
+    
+      Scanner sc = new Scanner(System.in);
+      double M = sc.nextDouble(); // original meal price
+      int T = sc.nextInt(); // tip percentage
+      int X = sc.nextInt(); // tax percentage
+      
+      double finalPrice = M + M * T/100 + M * X/100;        
+      int total = (int) Math.round(finalPrice);
+      System.out.println("The final price of the meal is $" + total + ".");
+
+      
+    }
+}
