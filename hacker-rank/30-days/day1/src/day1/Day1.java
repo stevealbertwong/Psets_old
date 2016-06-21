8d22fb832e33ece9fcb5242d75567337923ee3db
steveandrewwong <wongkwunkit@gmail.com>
2016-06-21 04:19:22 -0700

    import packages

--- hacker-rank/30-days/day1/src/day1/Day1.java
+++ hacker-rank/30-days/day1/src/day1/Day1.java
@@ -3,10 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package day1;
-import java.io.*; // scanner class
-import java.util.*;
+package day1; // user-defined packages
+
+// built-in packages
+import java.io.*; // 
+import java.util.*; // scanner, stack, HashMap, Map
 import java.text.*;
+import java.net.*; // url, socket
+import java.lang.*; // math, System.in ANYTHING IN JAVA/LANG WE DONT HAVE TO IMPORT
 import java.math.*; // java: library, math: package, *: all classes
 /**
  *
@@ -20,6 +24,10 @@ public class Day1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in); // read from std in 
+        String inputString = scan.nextLine();
+        scan.close();
+        
         
         
     }
