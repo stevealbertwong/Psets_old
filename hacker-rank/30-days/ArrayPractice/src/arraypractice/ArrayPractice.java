3977dc42fc7a4c66a5349cad95283881f9ea509d
steveandrewwong <wongkwunkit@gmail.com>
2016-06-22 09:55:08 -0700

    30 days array practice

---
+++ hacker-rank/30-days/ArrayPractice/src/arraypractice/ArrayPractice.java
@@ -0,0 +1,23 @@
+/*
+Array: reference data structure
+
+3 ways to initialize/declare arrays
+    1. int[] intArray1; 
+
+*/
+package arraypractice;
+
+/**
+ *
+ * @author SteveAndrewWong
+ */
+public class ArrayPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
--- hacker-rank/30-days/ReverseArray/src/reversearray/ReverseArray.java
+++ hacker-rank/30-days/ReverseArray/src/reversearray/ReverseArray.java
@@ -14,21 +14,38 @@ public class ReverseArray {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // create array
-        // parse poem.txt into lines  + for -- loop
-        Array arr <String> = new Array <String>;
-        
-        Scanner sc = new Scanner(new File(poem.txt));
-        while (sc.hasNextLine()){
-            int j = number of lines of poem.txt;
-            String line = sc.getline();
-            arr[j] = line;
-            j--;
 
-        for (int i = 0, i > arr.getsize(), i++){
-            System.out.print(arr[i]);
+//        Array arr <String> = new Array <String>;
+//        
+//        
+//        Scanner sc = new Scanner(new File(poem.txt));
+//        while (sc.hasNextLine()){
+//            int j = number of lines of poem.txt;
+//            String line = sc.getline();
+//            arr[j] = line;
+//            j--;
+//
+//        for (int i = 0, i > arr.getsize(), i++){
+//            System.out.print(arr[i]);
+//        }
+//    }
+        ArrayList <String> list = new ArrayList();
+        String filename = readLine("type a file name");
+        Scanner sc = new Scanner(new File(filename);
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+            list.add(line);
         }
-    }
+        
+        // simple for loop
+        // for (String line : list){ list.get(i)};
+        
+        for (int i=list.size() - 1 ; i>=0; i-- ){
+            list.get(i);
+        }
+            
+        }
+        
+
     
 }
