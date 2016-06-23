02c0aba4ccadfcfbb4944c832c11feb42f780e73
steveandrewwong <wongkwunkit@gmail.com>
2016-06-22 22:15:18 -0700

    30 days array

--- hacker-rank/30-days/ArrayPractice/src/arraypractice/ArrayPractice.java
+++ hacker-rank/30-days/ArrayPractice/src/arraypractice/ArrayPractice.java
@@ -1,23 +1,43 @@
 /*
-Array: reference data structure
+Array: reference data structure -> pointers and address
+thats why you "overwrite" its value
 
 3 ways to initialize/declare arrays
-    1. int[] intArray1; 
-
+    1. int[] intArray1;
+    2. int[] intArray2 = new int[4]; // size of array is 4 
+    3. int[] intArray3 = {1,2,3,4};
 */
 package arraypractice;
+
+import java.util.Arrays;
+
+
 
 /**
  *
  * @author SteveAndrewWong
  */
 public class ArrayPractice {
-
+    
+    public static void printArray(int[] array){
+       System.out.print(Arrays.toString(array) + "\n");
+}
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] intArray1;
+        int[] intArray3 = {1,2,3,4};
+        int[] intArray2 = new int[4];
+        int outOfBounds = intArray3[3];
+        System.out.print(Arrays.toString(intArray2) + "\n");
+        System.out.print(Arrays.toString(intArray3) + "\n");
+        printArray(intArray3);
+        System.out.print(outOfBounds + "\n");
+        
+        
+        
     }
     
 }
