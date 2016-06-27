9c2e6bdf8c6ec135e27d8a239b62523efdb649e4
steveandrewwong <wongkwunkit@gmail.com>
2016-06-27 04:26:31 -0700

    firebaseName.setValue(d-dog)

--- 193a/193login/app/src/main/java/com/example/steveandrewwong/a193login/MainActivity.java
+++ 193a/193login/app/src/main/java/com/example/steveandrewwong/a193login/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 // import stanford.andriodlib.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Firebase
+        Firebase.setAndroidContext(this); // set android context to be this activity
     }
 
     public void loginClick(View view) {
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         String name = nameEditText.getText().toString();
         EditText passwordEditText = (EditText) findViewById(R.id.password);
         String password = passwordEditText.getText().toString();
+
+        // look up this person's password in Firebase
+        Firebase firebase = new Firebase("https://amber-inferno-3012.firebaseio.com/");
+        Firebase fbName = firebase.child("simpsons").child("students").child("123").child("name"); // return a firebase
+        fbName.setValue("d-dog");
+
 
     }
 }
