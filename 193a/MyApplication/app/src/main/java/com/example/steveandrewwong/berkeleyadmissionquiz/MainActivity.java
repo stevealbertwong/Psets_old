f2562f2c8e92087d3523ab805df1aab35bcd3f95
steveandrewwong <wongkwunkit@gmail.com>
2016-06-30 08:51:26 -0700

    event onClick button = (Button) findViewById(R.id.buttom)

--- 193a/MyApplication/app/src/main/java/com/example/steveandrewwong/berkeleyadmissionquiz/MainActivity.java
+++ 193a/MyApplication/app/src/main/java/com/example/steveandrewwong/berkeleyadmissionquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.steveandrewwong.berkeleyadmissionquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,4 +17,13 @@ public class MainActivity extends AppCompatActivity {
     // convert the button's text into int and compare the 2 buttons
     // when the button is clicked -> compare e.g. if left is clicked and left int > right int + change the random number inside button's text
     // if true, get the int inside TextView point and increase by 1
+
+    public void leftButtomClicked(View view) {
+        Button leftButton = (Button) findViewById(R.id.button);
+    }
+
+
+    public void rightButtomClicked(View view) {
+        Button rightButton = (Button) findViewById(R.id.button2);
+    }
 }
