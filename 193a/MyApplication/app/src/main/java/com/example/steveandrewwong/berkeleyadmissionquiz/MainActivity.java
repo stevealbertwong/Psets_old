4d7036aec3bec339fe6e8e6a3ea98cac47125796
steveandrewwong <wongkwunkit@gmail.com>
2016-06-30 08:24:07 -0700

    193a layout, wigets

--- 193a/MyApplication/app/src/main/java/com/example/steveandrewwong/berkeleyadmissionquiz/MainActivity.java
+++ 193a/MyApplication/app/src/main/java/com/example/steveandrewwong/berkeleyadmissionquiz/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // randomly generate button's text
+    // convert the button's text into int and compare the 2 buttons
+    // when the button is clicked -> compare e.g. if left is clicked and left int > right int + change the random number inside button's text
+    // if true, get the int inside TextView point and increase by 1
 }
---
+++ 193a/NinjaTurtle/app/src/main/java/com/example/steveandrewwong/ninjaturtle/ninja.java
@@ -0,0 +1,13 @@
+package com.example.steveandrewwong.ninjaturtle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ninja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ninja);
+    }
+}
