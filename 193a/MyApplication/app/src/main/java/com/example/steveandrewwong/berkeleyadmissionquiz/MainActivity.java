6945a22922d0beecef5ba2e27c07e665aea57514
steveandrewwong <wongkwunkit@gmail.com>
2016-06-30 09:48:41 -0700

    random number setText()

--- 193a/MyApplication/app/src/main/java/com/example/steveandrewwong/berkeleyadmissionquiz/MainActivity.java
+++ 193a/MyApplication/app/src/main/java/com/example/steveandrewwong/berkeleyadmissionquiz/MainActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    // onCreate runs when Activity gets created
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // random number
+        Random randomNumber = new Random();
+        int num1 = randomNumber.nextInt(10);
+        int num2 = randomNumber.nextInt(10);
+
+        // view object -> button, textview, edittext + cast the return value to appropriate type
+        Button leftButton = (Button) findViewById(R.id.button);
+        Button rightButton = (Button) findViewById(R.id.button2);
+//        leftButton.setText(num1);
+//        rightButton.setText(num2);
     }
 
     // randomly generate button's text
@@ -18,12 +33,25 @@ public class MainActivity extends AppCompatActivity {
     // when the button is clicked -> compare e.g. if left is clicked and left int > right int + change the random number inside button's text
     // if true, get the int inside TextView point and increase by 1
 
-    public void leftButtomClicked(View view) {
-        Button leftButton = (Button) findViewById(R.id.button);
-    }
+//    public void leftButtomClicked(View view) {
+//        Button leftButton = (Button) findViewById(R.id.button);
+//        Button rightButton = (Button) findViewById(R.id.button2);
+//        TextView points = (TextView) findViewById(R.id.textView2);
+
+        // each widget has an associated java object i.e. get() set() methods that corresponds to the properties in Design View
+//        int leftNumber = (int) leftButton.getText();
+//        leftButton.get
+//        int rightNumber = rightButton.getText();
+//        if (leftNumber >= rightNumber){
+//            points++;
+//            toast("Correct");
+//
+//            leftButton.setText(); //
+//        };
+//    }
 
 
-    public void rightButtomClicked(View view) {
-        Button rightButton = (Button) findViewById(R.id.button2);
-    }
+//    public void rightButtomClicked(View view) {
+//        Button rightButton = (Button) findViewById(R.id.button2);
+//    }
 }
