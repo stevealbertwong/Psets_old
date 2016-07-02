af509fbcf43e476d130f35fbb9590eba9830448f
steveandrewwong <wongkwunkit@gmail.com>
2016-07-02 06:42:47 -0700

    193a AdapterArray ListView setOnItemClickListener

--- 193a/ListView/app/src/main/java/com/example/steveandrewwong/listview/MainActivity.java
+++ 193a/ListView/app/src/main/java/com/example/steveandrewwong/listview/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // 1st: Array -> 2nd ArrayAdapter -> 3rd ListView.setAdapter
     // dictionary, list of words in JSON format
     private static final String [] WORDS ={
             "abate", "to lessen to subside",
@@ -18,4 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
         // filter out the elements, odd is word, even is definition
     }
+
+    // Syntatic diarrhea 
+    // java does not pass the method as a parameter -> anonymous inner classes
+    // i.e. make an object that contains the function and pass that object as the parameter
+    // new AdapterView.OnItemClickListener(){public void onItemClick}
 }
