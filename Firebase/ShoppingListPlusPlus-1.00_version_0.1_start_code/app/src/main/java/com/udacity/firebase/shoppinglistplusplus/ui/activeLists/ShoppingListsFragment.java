d1d818f95dfc80ba4083260c36fb76466d909bd0
steveandrewwong <wongkwunkit@gmail.com>
2016-07-06 01:24:07 -0700

    shop++ textview.settext(datasnapshot.getvalue)

--- Firebase/ShoppingListPlusPlus-1.00_version_0.1_start_code/app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeLists/ShoppingListsFragment.java
+++ Firebase/ShoppingListPlusPlus-1.00_version_0.1_start_code/app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeLists/ShoppingListsFragment.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -23,7 +23,10 @@ import com.udacity.firebase.shoppinglistplusplus.R;
  * create an instance of this fragment.
  */
 public class ShoppingListsFragment extends Fragment {
+    // private since every activity is likely to have a ListView
     private ListView mListView;
+    // declare here since it is used in private void initialize screen
+    private TextView mTextViewListName;
 
     public ShoppingListsFragment() {
         /* Required empty public constructor */
@@ -66,15 +69,22 @@ public class ShoppingListsFragment extends Fragment {
         initializeScreen(rootView);
 
         // TODO: attach listener to ref to get data stored at listName child node of root node
-        // TODO:
+        // TODO: x constant ping + asyn adapter + asyn taks thread request respond model -> datasnapshot + listener: firebase pings phones w connected database
         // listName child node reference
         Firebase refListName = new Firebase("https://shoppingplusplus-f6a38.firebaseio.com/").child("listName");
         // see Marty's example: addChildEventListener
+        // attach listener to a specific node
+        // if attach to a parent node, any changes in child node will trigger listener code -> send parent node + all its children
+        // Firebase SDK deals with all threading and asychonizity
+        // ValueEventListener methods are called on the main or UI thread -> dont put expensive operations, otherwise slow
         refListName.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // dataSnapshot: listName child node
-                String value = (String) dataSnapshot.getValue();
+                // dataSnapshot: listName child node, a read-only copy
+                // listNameValue is the String user enter in AddListDialogFragment
+                String listNameValue = (String) dataSnapshot.getValue();
+                mTextViewListName.setText(listNameValue);
+
 
             }
 
@@ -82,7 +92,7 @@ public class ShoppingListsFragment extends Fragment {
             public void onCancelled(FirebaseError firebaseError) {
 
             }
-        })
+        });
 
 
 
@@ -106,9 +116,14 @@ public class ShoppingListsFragment extends Fragment {
 
 
     /**
-     * Link layout elements from XML
+     * TODO: Link layout elements from XML
      */
     private void initializeScreen(View rootView) {
+        // rootView: inflater.inflate(R.layout.fragment_shopping_lists, container, false)
+        // since fragment_shopping_lists.xml includes single_active_list.xml <include layout="@layout/single_active_list" />
         mListView = (ListView) rootView.findViewById(R.id.list_view_active_lists);
+        mTextViewListName = (TextView) rootView.findViewById(R.id.text_view_list_name);
+
+
     }
 }
